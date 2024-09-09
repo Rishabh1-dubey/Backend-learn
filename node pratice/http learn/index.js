e452689a29eb07a0821e822251ebaf917c374cfb
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-09-09 10:16:57 +0530

    did some pratice on exprss server

--- node pratice/http learn/index.js
+++ node pratice/http learn/index.js
@@ -1,6 +1,25 @@
  // <-------- http server ----------->
-const http= require("http");
-var server= http.createServer(function(req,res){
-    res.end("server connected wait some time .............")
+// const http= require("http");
+// var server= http.createServer(function(req,res){
+//     res.end("server connected wait some time .............")
+// })
+// server.listen(3000);
+
+
+//express and postman pratice
+
+const express = require("express");
+const app = express();
+ var data=[1,2,3,4,5]
+app.get("/", function(req,res){
+    res.send("hey")
 })
-server.listen(3000);
+
+app.get('/data',function(req,res){
+    res.send("number is assigne to data")
+})
+app.post("/data/:number",(req,res)=>{
+    data.push(parseInt(req.params.number));
+    res.send(data);
+}) 
+app.listen(3000);
