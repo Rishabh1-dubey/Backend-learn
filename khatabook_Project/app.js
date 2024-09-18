93613c676a79f14320f8c9a318da6b005fd31ad9
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-09-18 09:49:57 +0530

    update section is completed

--- khatabook_Project/app.js
+++ khatabook_Project/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const fs= require("fs")
+const fs= require("fs");
+const { isUtf8 } = require("buffer");
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.json())
@@ -13,8 +14,25 @@ app.get("/", function(req,res){
         res.render("index",{files})
     })
   
-     
+  
+    //whenever you render anything you have to crate view file
 })
+
+app.get("/edit/:filename", function(req,res){
+    fs.readFile(`./files/${req.params.filename}`,"utf-8" , function(err,data){
+         if(err)  return res.send(err);
+        res.render("edit",{data,filename:req.params.filename})
+    })
+    })
+    app.post("/update/:filename", function(req,res){
+    fs.writeFile(`./files/${req.params.filename}`, req.body.filedata,function(err){
+         if(err)  return res.send(err);
+        res.redirect("/")
+    })
+    })
+  
+
+    //whenever you render anything you have to crate view file
 // app.get("/create", function(req,res){
     //const currentDate = new Date();
 // 
